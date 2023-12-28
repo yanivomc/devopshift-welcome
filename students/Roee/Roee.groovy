78231b9eef2950a105516c76e2d34b9629c8f126
Roee <Roee@example.com>
2023-12-28 09:36:47 +0000

    Added dsl groovy file

---
+++ students/Roee/Roee.groovy
@@ -0,0 +1,18 @@
+job('Roee') { // Job NAME
+    scm { // Configure Source control management 
+        git('https://github.com/yanivomc/devopshift-welcome.git', '*/elbit/jenkinsdec26') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@domain.com')
+gi        }
+    }
+    triggers { // Configure when to check for changes 
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps { // what steps to take 
+        shell("npm install")
+    }
+}
