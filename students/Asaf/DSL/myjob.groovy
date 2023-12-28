d59be170219fb1dfbcc8611c39b339fb65985b60
Your Name <you@example.com>
2023-12-28 09:25:34 +0000

    update

--- students/Asaf/DSL/myjob.groovy
+++ students/Asaf/DSL/myjob.groovy
@@ -13,6 +13,6 @@ job('NodeJS example') { // Job NAME
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps { // what steps to take 
-        shell(echo “Test 1”)
+        shell("echo “Test 1"")
     }
 }
