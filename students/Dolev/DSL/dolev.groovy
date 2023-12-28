52d58231aa5976a4b8c7d1800b808938e14c9e47
Your Name <you@example.com>
2023-12-28 09:22:01 +0000

    dsl-update

---
+++ students/Dolev/DSL/dolev.groovy
@@ -0,0 +1,21 @@
+job('NodeJS example') { // Job NAME
+    scm { // Configure Source control management 
+        git('https://github.com/yanivomc/devopshift-welcome.git') {  node -> // is hudson.plugins.git.GitSCM
+            // Specify the branches to examine for changes and to build.
+            //https://jenkinsci.github.io/job-dsl-plugin/#method/javaposse.jobdsl.dsl.jobs.FreeStyleJob.scm
+            branch('elbit/jenkinsdec26')
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@domain.com)
+        }
+    }
+    triggers { // Configure when to check for changes 
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps { // what steps to take 
+        shell("npm install")
+    }
+}
