444298c4e3dc291e7acd0e10b826934d58da2048
Your Name <you@example.com>
2023-12-28 09:48:39 +0000

    add nast

---
+++ students/anastasia/dsl/nast.groovy
@@ -0,0 +1,21 @@
+job('NodeJS example') { // Job NAME
+    scm { // Configure Source control management 
+        git('http://github.com/yanivomc/devopshift-welcome.git', '*/elbit/jenkinsdec26') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@domain.com')
+        }
+    }
+    triggers { // Configure when to check for changes 
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps { // what steps to take 
+        shell(echo “Test 1”)
+        shell(echo “ install packages”)
+        shell("npm install")
+        shell(echo “Running unit tests”)
+    }
+}
