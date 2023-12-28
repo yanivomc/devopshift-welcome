342c5ec1b8965ada2914313001214170983973d4
Your Name <you@example.com>
2023-12-28 09:20:04 +0000

    g

--- students/Gil/Mygroovy.groovy
+++ students/Gil/Mygroovy.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') { // Job NAME
     scm { // Configure Source control management 
-        git('git://github.com/yanivomc/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/yanivomc/devopshift-welcome.git','elbit/jenkinsdec26') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@domain.com)
+            node / gitConfigEmail('jenkins-dsl@domain.com')
         }
     }
     triggers { // Configure when to check for changes 
