5b5e472edd60ba86fe47fafd16f8635d0b7b8c10
Ori Arad <you@example.com>
2023-12-28 09:05:47 +0000

    Fix typo

--- students/ori/dsl.groovy
+++ students/ori/dsl.groovy
@@ -2,7 +2,7 @@ job('NodeJS example') { // Job NAME
     scm { // Configure Source control management 
         git('git://github.com/yanivomc/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@domain.com)
+            node / gitConfigEmail('jenkins-dsl@domain.com')
         }
     }
     triggers { // Configure when to check for changes 
