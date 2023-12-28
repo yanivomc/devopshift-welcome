7a23bb3bb87813affc463e1f67fdd25c0eabd708
Your Name <you@example.com>
2023-12-28 09:22:18 +0000

    lab02

---
+++ students/olga/Mygroovy.groovy
@@ -0,0 +1,18 @@
+job('NodeJsLab01') { // Job NAME
+    scm { // Configure Source control management 
+        git('https://github.com/yanivomc/devopshift-welcome.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@domain.com')
+        }
+    }
+    triggers { // Configure when to check for changes 
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('NodeJS21.4') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps { // what steps to take 
+        shell("npm install")
+    }
+}
