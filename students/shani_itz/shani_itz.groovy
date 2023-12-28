f8005972d5a238bcf2ffa25f5cab96f6f060d9bc
Your Name <you@example.com>
2023-12-28 10:49:33 +0000

    updates

--- students/shani_itz/shani_itz.groovy
+++ students/shani_itz/shani_itz.groovy
@@ -1,21 +1,18 @@
-job('Groovy example') { // Job NAME
+job('NodeJS example') { // Job NAME
     scm { // Configure Source control management 
-        git('git:https://github.com/yanivomc/devopshift-welcome.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/yanivomc/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@domain.com')
         }
     }
     triggers { // Configure when to check for changes 
-        scm('H/2 * * * *')
+        scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('NodeJS') // this is the name of the NodeJS installation in 
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps { // what steps to take 
-        shell("echo "Test 1"")
-        shell("echo "install packages"")
         shell("npm install")
-        shell("echo "Running unit tests"")
     }
 }
