022f15869ab34ecd59ed2c30056ecf3a6bc92b53
Your Name <you@example.com>
2024-01-03 09:56:40 +0000

    111

---
+++ students/Roy/DSL/dslpipeline.groovy
@@ -0,0 +1,26 @@
+
+pipelineJob('projectx') { // Job NAME
+    definition {
+        cpsScm {
+            scm {
+                git('https://github.com/yanivomc/devopshift-welcome.git') { // Your repository
+                    branches('elbit/jenkinsdec26') // Branch to build, replace with your branch if needed
+                    // extensions {
+                    //     // relativeTargetDirectory('docker-demo') // Optional: Check out to a sub-directory
+                    //     // cleanBeforeCheckout() // Optional: Clean the workspace before checkout
+                    // }
+                    userRemoteConfigs {
+                        userRemoteConfig {
+                            name('Roy')
+                            email('roysadan@domain.com')
+                        }
+                    }
+                }
+            }
+            scriptPath('./students/Roy/pipeline/projectX/Jenkinsfile') // Path to the Jenkinsfile in the repository
+        }
+    }
+    triggers { // Configure when to check for changes 
+        scm('H/5 * * * *')
+    }
+}
