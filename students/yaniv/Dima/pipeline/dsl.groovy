9f8e120c322411c7bf805af0767b9df9c0ef93e5
Dima <you@example.com>
2024-01-03 10:09:45 +0000

    update dsl

--- students/yaniv/Dima/pipeline/dsl.groovy
+++ students/yaniv/Dima/pipeline/dsl.groovy
@@ -17,25 +17,51 @@ job('Pipeline example') { // Job NAME
     }
 }
 
-pipelineJob('projectx') { // Job NAME
-   definition {
-       cpsScm {
-           scm {
-               git('https://github.com/yanivomc/devopshift-welcome.git') { // Your repository
-                   branches('elbit/jenkinsdec26') // Branch to build, replace with your branch if needed
-                   
-                   userRemoteConfigs {
-                       userRemoteConfig {
-                           name('DSL User')
-                           email('jenkins-dsl@domain.com')
-                       }
-                   }
-               }
-           }
-           scriptPath('./students/yaniv/Dima/Jenkinsfile/Jenkinsfile') // Path to the Jenkinsfile in the repository
+//pipelineJob('projectx') { // Job NAME
+//   definition {
+//       cpsScm {
+//           scm {
+//              git('https://github.com/yanivomc/devopshift-welcome.git') { // Your repository
+//                   branches('elbit/jenkinsdec26') // Branch to build, replace with your branch if needed
+//                   
+//                   userRemoteConfigs {
+//                       userRemoteConfig {
+//                           name('DSL User')
+//                           email('jenkins-dsl@domain.com')
+//                       }
+//                   }
+//               }
+//           }
+//           scriptPath('./students/yaniv/Dima/Jenkinsfile/Jenkinsfile') // Path to the Jenkinsfile in the repository
+//       }
+//   }
+//   triggers { // Configure when to check for changes
+//       scm('H/5 * * * *')
+//   }
+//}
+
+pipelineJob('my-pipeline2') { // broken branch config
+ def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
+
+
+ triggers {
+   scm('H/5 * * * *')
+ }
+ description("My Pipeline 2")
+
+
+ definition {
+   cpsScm {
+     scm {
+       git {
+         remote { url(repo) }
+         branches('elbit/jenkinsdec26') // this is fine
+         scriptPath('./students/yaniv/Dima/Jenkinsfile/Jenkinsfile')
+         extensions { }  // required as otherwise it may try to tag the repo, which you may not want
        }
+     }
    }
-   triggers { // Configure when to check for changes
-       scm('H/5 * * * *')
-   }
+ }
 }
+
+
