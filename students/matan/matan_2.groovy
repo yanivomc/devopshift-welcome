a60bb41c17f9941338e37dc4c201d54c5df28d31
Matan <you@example.com>
2024-01-03 10:05:30 +0000

    dsfsdfs

--- students/matan/matan_2.groovy
+++ students/matan/matan_2.groovy
@@ -1,25 +1,23 @@
-pipelineJob('קובץ dsa') { // Job NAME
-   definition {
-       cpsScm {
-           scm {
-               git('https://github.com/yanivomc/devopshift-welcome.git') { // Your repository
-                   branches('*/main') // Branch to build, replace with your branch if needed
-                   extensions {
-                       relativeTargetDirectory('docker-demo') // Optional: Check out to a sub-directory
-                       cleanBeforeCheckout() // Optional: Clean the workspace before checkout
-                   }
-                   userRemoteConfigs {
-                       userRemoteConfig {
-                           name('DSL User')
-                           email('jenkins-dsl@domain.com')
-                       }
-                   }
-               }
-           }
-           scriptPath('/sudent/matan/jenkinsfile') // Path to the Jenkinsfile in the repository
+pipelineJob('קובץ dsa') { // broken branch config
+ def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
+
+
+ triggers {
+   scm('H/5 * * * *')
+ }
+ description("My Pipeline 2")
+
+
+ definition {
+   cpsScm {
+     scm {
+       git {
+         remote { url(repo) }
+         branches('elbit/jenkinsdec26') // this is fine
+         scriptPath('students/???????FOLDER/jenkinsFile101/jenkinsfile')
+         extensions { }  // required as otherwise it may try to tag the repo, which you may not want
        }
+     }
    }
-   triggers { // Configure when to check for changes
-       scm('H/5 * * * *')
-   }
+ }
 }
