1bc41fca2d904cd89ed8c92c11a7e755ac052c08
Your Name <you@example.com>
2024-01-03 10:28:44 +0000

    formatting

--- students/ori/dsl/pipelinedsl.groovy
+++ students/ori/dsl/pipelinedsl.groovy
@@ -1,18 +1,14 @@
 pipelineJob('My Generated Job') { // Job NAME
-   definition {
-       cpsScm {
-           scm {
+    definition {
+        cpsScm {
+            scm {
                 git {
                     remote { url('https://github.com/yanivomc/devopshift-welcome.git') }
                     branches('elbit/jenkinsdec26') // this is fine
                     scriptPath('students/yaniv/repo/projectx/jenkinsfile')
                     extensions { }  // required as otherwise it may try to tag the repo, which you may not want
                 }  
-           }
-           //scriptPath('students/ori/pipelines/day030124/jenkinsfile') // Path to the Jenkinsfile in the repository
+           }           
        }
-   }
-   triggers { // Configure when to check for changes
-       scm('H/5 * * * *')
    }
 }
