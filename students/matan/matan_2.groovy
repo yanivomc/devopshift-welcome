d89e1698fae88902c3b34548235da444b8682647
Matan <you@example.com>
2024-01-03 09:53:48 +0000

    create groovy file

---
+++ students/matan/matan_2.groovy
@@ -0,0 +1,25 @@
+pipelineJob('projectx') { // Job NAME
+   definition {
+       cpsScm {
+           scm {
+               git('https://github.com/yanivomc/devopshift-welcome.git') { // Your repository
+                   branches('*/main') // Branch to build, replace with your branch if needed
+                   extensions {
+                       relativeTargetDirectory('docker-demo') // Optional: Check out to a sub-directory
+                       cleanBeforeCheckout() // Optional: Clean the workspace before checkout
+                   }
+                   userRemoteConfigs {
+                       userRemoteConfig {
+                           name('DSL User')
+                           email('jenkins-dsl@domain.com')
+                       }
+                   }
+               }
+           }
+           scriptPath('Jenkinsfile') // Path to the Jenkinsfile in the repository
+       }
+   }
+   triggers { // Configure when to check for changes
+       scm('H/5 * * * *')
+   }
+}
