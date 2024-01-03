71834af7b6f4e26aaf985e164280e4f8edc98b89
asaf <asaf@gmail.com>
2024-01-03 10:25:34 +0000

    update

--- students/Asaf/DSL/dslpipeline.groovy
+++ students/Asaf/DSL/dslpipeline.groovy
@@ -34,7 +34,7 @@ pipelineJob('projectx') { // broken branch config
        git {
          remote { url(repo) }
          branches('elbit/jenkinsdec26') // this is fine
-         scriptPath('students/yaniv/repo/projectx/jenkinsfile')
+         scriptPath('students/Asaf/Repo/jenkinsfile')
          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
        }
      }
@@ -44,27 +44,3 @@ pipelineJob('projectx') { // broken branch config
 
 
 
-// TEAM A PROJECT Y
-pipelineJob('projecty') { // broken branch config
- def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
-
-
- triggers {
-   scm('H/5 * * * *')
- }
- description("My Pipeline 2")
-
-
- definition {
-   cpsScm {
-     scm {
-       git {
-         remote { url(repo) }
-         branches('elbit/jenkinsdec26') // this is fine
-         scriptPath('students/yaniv/repo/projecty/jenkinsfile')
-         extensions { }  // required as otherwise it may try to tag the repo, which you may not want
-       }
-     }
-   }
- }
-}
