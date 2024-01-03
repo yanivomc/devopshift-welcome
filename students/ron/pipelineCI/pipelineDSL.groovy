b902afef829fcacaeb339deb9f6d859fdce170a5
Ron Huberfeld <ron.huberfeld@gmail.com>
2024-01-03 09:58:42 +0000

    modify script

--- students/ron/pipelineCI/pipelineDSL.groovy
+++ students/ron/pipelineCI/pipelineDSL.groovy
@@ -1,8 +1,24 @@
 pipelineJob('pipeline-ci') {
+    def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
+
+    triggers { // Configure when to check for changes
+        scm('H/5 * * * *')
+    }
     definition {
-        cps {
-            script(readFileFromWorkspace('students/ron/Jenkinsfile'))
-            sandbox()
+        cpsScm {
+            scm {
+                git {
+                    remote { url(repo) }
+                    branches('elbit/jenkinsdec26')
+                    userRemoteConfigs {
+                        userRemoteConfig {
+                            name('DSL User')
+                            email('jenkins-dsl@domain.com')
+                        }
+                    }
+                }
+            }
         }
+        scriptPath('students/ron/Jenkinsfile')
     }
 }
