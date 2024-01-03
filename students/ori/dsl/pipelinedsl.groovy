94b06ac92de67d786ffc2821760cf227bc366f0f
Your Name <you@example.com>
2024-01-03 10:08:13 +0000

    fix branch

--- students/ori/dsl/pipelinedsl.groovy
+++ students/ori/dsl/pipelinedsl.groovy
@@ -2,14 +2,8 @@ pipelineJob('projectx') { // Job NAME
    definition {
        cpsScm {
            scm {
-               git('https://github.com/yanivomc/devopshift-welcome.git') { 
-                   branches('elbit/jenkinsdec26')
-                   userRemoteConfigs {
-                       userRemoteConfig {
-                           name('DSL User')
-                           email('jenkins-dsl@domain.com')
-                       }
-                   }
+               git('https://github.com/yanivomc/devopshift-welcome.git', '*/elbit/jenkinsdec26') { 
+
                }
            }
            scriptPath('students/ori/pipelines/day030124/jenkinsfile') // Path to the Jenkinsfile in the repository
