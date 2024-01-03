cc372b230117dd800ae2f3b4347743dcdadc818e
Yaniv Cohen <yanivomc@gmail.com>
2024-01-03 10:34:11 +0000

    updates

--- students/yaniv/DSL/INTRO/pipeline/dslpipeline.groovy
+++ students/yaniv/DSL/INTRO/pipeline/dslpipeline.groovy
@@ -1,9 +1,9 @@
 job('NodeJS example') { // Job NAME
     scm { // Configure Source control management 
         git('git://github.com/yanivomc/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@domain.com')
-        }
+        }            node / gitConfigName('DSL User')
+
     }
     triggers { // Configure when to check for changes 
         scm('H/5 * * * *')
