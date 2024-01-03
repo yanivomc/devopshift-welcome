b094d2da2403df42cd1e7bb011c186b80d2bde09
Your Name <you@example.com>
2024-01-03 10:25:01 +0000

    try to fix tag creation issue

--- students/ori/dsl/pipelinedsl.groovy
+++ students/ori/dsl/pipelinedsl.groovy
@@ -2,11 +2,14 @@ pipelineJob('My Generated Job') { // Job NAME
    definition {
        cpsScm {
            scm {
-               git('https://github.com/yanivomc/devopshift-welcome.git', '*/elbit/jenkinsdec26') { 
-
-               }
+                git {
+                    remote { url('https://github.com/yanivomc/devopshift-welcome.git') }
+                    branches('elbit/jenkinsdec26') // this is fine
+                    scriptPath('students/yaniv/repo/projectx/jenkinsfile')
+                    extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+                }  
            }
-           scriptPath('students/ori/pipelines/day030124/jenkinsfile') // Path to the Jenkinsfile in the repository
+           //scriptPath('students/ori/pipelines/day030124/jenkinsfile') // Path to the Jenkinsfile in the repository
        }
    }
    triggers { // Configure when to check for changes
