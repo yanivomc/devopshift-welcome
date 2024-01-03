7d41daa27536ee925c99c32e6fd01f0aa5af3ce6
Ron Huberfeld <ron.huberfeld@gmail.com>
2024-01-03 10:17:22 +0000

    add specific project

--- students/ron/pipelineCI/pipelineDSL.groovy
+++ students/ron/pipelineCI/pipelineDSL.groovy
@@ -1,4 +1,4 @@
-pipelineJob('pipeline-ci') {
+pipelineJob('Generic Project') {
     def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
 
     triggers { // Configure when to check for changes
@@ -11,6 +11,26 @@ pipelineJob('pipeline-ci') {
                     remote { url(repo) }
                     branches('elbit/jenkinsdec26')
                     scriptPath('students/ron/Jenkinsfile')
+                    extensions {}
+                }
+            }
+        }
+    }
+}
+
+pipelineJob('Project1') {
+    def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
+
+    triggers { // Configure when to check for changes
+        scm('H/5 * * * *')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote { url(repo) }
+                    branches('elbit/jenkinsdec26')
+                    scriptPath('students/ron/projects/project1/Jenkinsfile')
                     extensions {}
                 }
             }
