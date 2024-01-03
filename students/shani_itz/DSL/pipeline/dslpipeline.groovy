ba35437bcba5c9430ed5239fc2c9d04f422c178f
Your Name <shani@gmail.com>
2024-01-03 10:12:52 +0000

    final

--- students/shani_itz/DSL/pipeline/dslpipeline.groovy
+++ students/shani_itz/DSL/pipeline/dslpipeline.groovy
@@ -1,25 +1,41 @@
-pipelineJob('projectx') { // Job NAME
-   definition {
-       cpsScm {
-           scm {
-               git('https://github.com/yanivomc/devopshift-welcome.git') { // Your repository
-                   branches(' elbit/jenkinsdec26') // Branch to build, replace with your branch if needed
-                //    extensions {
-                //        relativeTargetDirectory('docker-demo') // Optional: Check out to a sub-directory
-                //        cleanBeforeCheckout() // Optional: Clean the workspace before checkout
-                //    }
-                   userRemoteConfigs {
-                       userRemoteConfig {
-                           name('shani')
-                           email('shani@gmail.com')
-                       }
-                   }
-               }
-           }
-           scriptPath('students/shani_itz/DSL/pipeline/JenkinsFile') // Path to the Jenkinsfile in the repository
+job('NodeJS example') { // Job NAME
+    scm { // Configure Source control management 
+        git('https://github.com/yanivomc/devopshift-welcome.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('shani')
+            node / gitConfigEmail('shani@gmail.com')
+        }
+    }
+    triggers { // Configure when to check for changes 
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('NodeJS') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps { // what steps to take 
+        shell("npm install")
+    }
+}
+pipelineJob('projectx') { // broken branch config
+ def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
+
+
+ triggers {
+   scm('H/5 * * * *')
+ }
+ description("My Pipeline 2")
+
+
+ definition {
+   cpsScm {
+     scm {
+       git {
+         remote { url(repo) }
+         branches('elbit/jenkinsdec26') // this is fine
+         scriptPath('students/shani_itz/DSL/pipeline/JenkinsFile')
+         extensions { }  // required as otherwise it may try to tag the repo, which you may not want
        }
+     }
    }
-   triggers { // Configure when to check for changes
-       scm('H/5 * * * *')
-   }
+ }
 }
