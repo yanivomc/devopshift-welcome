d9848980105719c82da7a22a037c72dc9f30dcf4
Your Name <shani@gmail.com>
2024-01-03 09:48:26 +0000

    update1

--- students/shani_itz/DSL/shani_itz.groovy
+++ students/shani_itz/DSL/shani_itz.groovy
@@ -1,18 +1,25 @@
-job('Shani NodeJS example') { // Job NAME
-    scm { // Configure Source control management 
-        git('https://github.com/yanivomc/devopshift-welcome.git', 'elbit/jenkinsdec26') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@domain.com')
-        }
-    }
-    triggers { // Configure when to check for changes 
-        scm('H/5 * * * *')
-    }
-    wrappers {
-        nodejs('NodeJS') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-    }
-    steps { // what steps to take 
-        shell("npm install")
-    }
+pipelineJob('projectx') { // Job NAME
+   definition {
+       cpsScm {
+           scm {
+               git('https://github.com/yanivomc/devopshift-welcome.git') { // Your repository
+                   branches(' elbit/jenkinsdec26') // Branch to build, replace with your branch if needed
+                //    extensions {
+                //        relativeTargetDirectory('docker-demo') // Optional: Check out to a sub-directory
+                //        cleanBeforeCheckout() // Optional: Clean the workspace before checkout
+                //    }
+                   userRemoteConfigs {
+                       userRemoteConfig {
+                           name('shani')
+                           email('shani@gmail.com')
+                       }
+                   }
+               }
+           }
+           scriptPath('students/shani_itz/pipeline/JenkinsFile') // Path to the Jenkinsfile in the repository
+       }
+   }
+   triggers { // Configure when to check for changes
+       scm('H/5 * * * *')
+   }
 }
