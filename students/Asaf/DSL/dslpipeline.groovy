167c4250517e6d1b1fc5105d10a422d613723003
asaf <asaf@gmail.com>
2024-01-03 09:55:46 +0000

    update

---
+++ students/Asaf/DSL/dslpipeline.groovy
@@ -0,0 +1,44 @@
+job('NodeJS example') { // Job NAME
+    scm { // Configure Source control management 
+        git('http://github.com/yanivomc/devopshift-welcome.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@domain.com')
+        }
+    }
+    triggers { // Configure when to check for changes 
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps { // what steps to take 
+        shell("npm install")
+    }
+}
+
+pipelineJob('projectx') { // Job NAME
+   definition {
+       cpsScm {
+           scm {
+               git('https://github.com/yanivomc/devopshift-welcome.git') { // Your repository
+                   branches('*/main') // Branch to build, replace with your branch if needed
+                   extensions {
+                       relativeTargetDirectory('docker-demo') // Optional: Check out to a sub-directory
+                       cleanBeforeCheckout() // Optional: Clean the workspace before checkout
+                   }
+                   userRemoteConfigs {
+                       userRemoteConfig {
+                           name('DSL User')
+                           email('jenkins-dsl@domain.com')
+                       }
+                   }
+               }
+           }
+           scriptPath('Jenkinsfile') // Path to the Jenkinsfile in the repository
+       }
+   }
+   triggers { // Configure when to check for changes
+       scm('H/5 * * * *')
+   }
+}
