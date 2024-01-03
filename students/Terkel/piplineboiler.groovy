60c9b2b7f4321995b042243618271fd50317ef2f
Terkel <terkmail@gmail.com>
2024-01-03 09:45:12 +0000

    update

--- students/Terkel/piplineboiler.groovy
+++ students/Terkel/piplineboiler.groovy
@@ -9,12 +9,25 @@ pipelineJob('my-pipeline') {
   definition {
     cpsScm {
       scm {
-        git {
-          remote { url(repo) }
-          branches('elbit/jenkinsdec26')
-          scriptPath('students/Terkel/jenkinsFile101/jenkinsfile')
-          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
-        }
+        // git {
+        //   remote { url(repo) }
+        //   branches('elbit/jenkinsdec26')
+        //   scriptPath('students/Terkel/jenkinsFile101/jenkinsfile')
+        //   extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        // }
+        git(url(repo)) { // Your repository
+            branches('elbit/jenkinsdec26') // Branch to build, replace with your branch if needed
+            extensions {
+                relativeTargetDirectory('docker-demo') // Optional: Check out to a sub-directory
+                cleanBeforeCheckout() // Optional: Clean the workspace before checkout
+            }
+            userRemoteConfigs {
+                userRemoteConfig {
+                    name('DSL User')
+                    email('jenkins-dsl@domain.com')
+                }
+            }
+    }
       }
     }
   }
