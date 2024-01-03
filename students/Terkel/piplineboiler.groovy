e1c6daf516bdd4870cc79c1ac606ae1c4272c369
Terkel <terkmail@gmail.com>
2024-01-03 09:38:33 +0000

    2 jobs

--- students/Terkel/piplineboiler.groovy
+++ students/Terkel/piplineboiler.groovy
@@ -19,3 +19,25 @@ pipelineJob('my-pipeline') {
     }
   }
 }
+
+pipelineJob('my-pipeline2') {
+  def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
+
+  triggers {
+    scm('H/5 * * * *')
+  }
+  description("My Pipeline 2")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('elbit/jenkinsdec26')
+          scriptPath('students/Terkel/jenkinsFile101/jenkinsfile')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+      }
+    }
+  }
+}
