1c23589259aaf77744b38112b6ee46445ac1f23f
Ori Arad <you@example.com>
2024-01-08 07:11:18 +0000

    fix script path

--- students/ori/dsl/pipelinedsl.groovy
+++ students/ori/dsl/pipelinedsl.groovy
@@ -5,7 +5,7 @@ pipelineJob('My Generated Job') { // Job NAME
                 git {
                     remote { url('https://github.com/yanivomc/devopshift-welcome.git') }
                     branches('elbit/jenkinsdec26') // this is fine
-                    scriptPath('students/yaniv/repo/projectx/jenkinsfile')
+                    scriptPath('students/ori/pipelines/day030124/jenkinsfile')
                     extensions { }  // required as otherwise it may try to tag the repo, which you may not want
                 }  
            }           
