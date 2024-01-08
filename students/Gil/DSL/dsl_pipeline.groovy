e41ebe035283e634abd50b399accdb4ad1e969cc
Your Name <you@example.com>
2024-01-08 08:31:04 +0000

    g

--- students/Gil/DSL/dsl_pipeline.groovy
+++ students/Gil/DSL/dsl_pipeline.groovy
@@ -41,3 +41,27 @@ pipelineJob('projectx') { // broken branch config
    }
  }
 }
+
+pipelineJob('flaskApp') { // broken branch config
+ def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
+
+
+ triggers {
+   scm('H/5 * * * *')
+ }
+ description("My Pipeline 2")
+
+
+ definition {
+   cpsScm {
+     scm {
+       git {
+         remote { url(repo) }
+         branches('elbit/jenkinsdec26') // this is fine
+         scriptPath('students/Gil/repo/python-flask/jenkinsfile')
+         extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+       }
+     }
+   }
+ }
+}
