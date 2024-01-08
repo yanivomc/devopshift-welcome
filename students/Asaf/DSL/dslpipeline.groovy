3a9107b11c4d6d90f59e38a9d332d9889443e5b5
Your Name <you@example.com>
2024-01-08 07:42:39 +0000

    8.1.2024

--- students/Asaf/DSL/dslpipeline.groovy
+++ students/Asaf/DSL/dslpipeline.groovy
@@ -1,9 +1,9 @@
 job('NodeJS example') { // Job NAME
     scm { // Configure Source control management 
         git('git://github.com/yanivomc/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@domain.com')
-        }
+        }            node / gitConfigName('DSL User')
+
     }
     triggers { // Configure when to check for changes 
         scm('H/5 * * * *')
@@ -34,7 +34,7 @@ pipelineJob('projectx') { // broken branch config
        git {
          remote { url(repo) }
          branches('elbit/jenkinsdec26') // this is fine
-         scriptPath('students/Asaf/Repo/jenkinsfile')
+         scriptPath('students/yaniv/repo/projectx/jenkinsfile')
          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
        }
      }
@@ -44,3 +44,27 @@ pipelineJob('projectx') { // broken branch config
 
 
 
+// TEAM A PROJECT Y
+pipelineJob('projecty') { // broken branch config
+ def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
+
+
+ triggers {
+   scm('H/5 * * * *')
+ }
+ description("My Pipeline 2")
+
+
+ definition {
+   cpsScm {
+     scm {
+       git {
+         remote { url(repo) }
+         branches('elbit/jenkinsdec26') // this is fine
+         scriptPath('students/yaniv/repo/projecty/jenkinsfile')
+         extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+       }
+     }
+   }
+ }
+}
