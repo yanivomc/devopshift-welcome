3f80c1c5ae2d1d4cf1c7bbf24b471e18af599f08
Your Name <you@example.com>
2024-01-08 08:29:57 +0000

    edit dsl

--- students/Orel/DSL/INTRO/pipeline/dslpipeline.groovy
+++ students/Orel/DSL/INTRO/pipeline/dslpipeline.groovy
@@ -22,3 +22,28 @@ pipelineJob('projectx') { // broken branch config
    }
  }
 }
+
+// TEAM A PROJECT flask
+pipelineJob('projectFlask') { // broken branch config
+ def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
+
+
+ triggers {
+   scm('H/5 * * * *')
+ }
+ description("My Pipeline 3")
+
+
+ definition {
+   cpsScm {
+     scm {
+       git {
+         remote { url(repo) }
+         branches('labs') // this is fine
+         scriptPath('students/Orel/repo/python-flask/jenkinsfile')
+         extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+       }
+     }
+   }
+ }
+}
