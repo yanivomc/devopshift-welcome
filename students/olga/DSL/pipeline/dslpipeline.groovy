4d76d14aa30dc40376f2f0e78b65f109f3a0d843
Your Name <you@example.com>
2024-01-08 07:37:55 +0000

    1

---
+++ students/olga/DSL/pipeline/dslpipeline.groovy
@@ -0,0 +1,24 @@
+// TEAM A PROJECT X 
+pipelineJob('projectx') { // broken branch config
+ def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
+
+
+ triggers {
+   scm('H/5 * * * *')
+ }
+ description("My Pipeline 2")
+
+
+ definition {
+   cpsScm {
+     scm {
+       git {
+         remote { url(repo) }
+         branches('elbit/jenkinsdec26') // this is fine
+         scriptPath('students/olga/repo/projectx/jenkinsfile')
+         extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+       }
+     }
+   }
+ }
+}
