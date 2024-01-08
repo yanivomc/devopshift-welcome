3fa21a24022e1982d66c146f623aee35b1729bf1
Terkel <terkmail@gmail.com>
2024-01-08 07:03:10 +0000

    removed broken piple

--- students/Terkel/piplineboiler.groovy
+++ students/Terkel/piplineboiler.groovy
@@ -1,30 +1,3 @@
-pipelineJob('my-pipeline-broken') { // broken branch config
-  def myrepo = 'https://github.com/yanivomc/devopshift-welcome.git'
-  def myname = 'Terkel'
-  def mymail = 'terkmail@gmail.com'
-  triggers {
-    scm('H/5 * * * *')
-  }
-  description("My Pipeline")
-
-  definition {
-    cpsScm {
-      scm {
-        git(myrepo) { // Your repository
-            branches('elbit/jenkinsdec26') // this is not fine
-            userRemoteConfigs {
-                userRemoteConfig {
-                    name(myname)
-                    email(mymail)
-                }
-            }
-    }
-      }
-      scriptPath('students/Terkel/jenkinsFile101/jenkinsfile') 
-    }
-  }
-}
-
 pipelineJob('my-pipeline') { // ok
   def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
 
