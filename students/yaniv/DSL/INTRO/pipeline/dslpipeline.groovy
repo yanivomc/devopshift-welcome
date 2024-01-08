b912636df90b7cb66ecccee9cd3446f843890264
Your Name <you@example.com>
2024-01-08 08:30:11 +0000

    updates

--- students/yaniv/DSL/INTRO/pipeline/dslpipeline.groovy
+++ students/yaniv/DSL/INTRO/pipeline/dslpipeline.groovy
@@ -22,3 +22,29 @@ pipelineJob('projectx') { // broken branch config
    }
  }
 }
+
+
+// TEAM A PROJECT flask 
+pipelineJob('flaskApp') { // broken branch config
+ def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
+
+
+ triggers {
+   scm('H/5 * * * *')
+ }
+ description("My flask app pipeline")
+
+
+ definition {
+   cpsScm {
+     scm {
+       git {
+         remote { url(repo) }
+         branches('elbit/jenkinsdec26') // this is fine
+         scriptPath('students/yaniv/repo/projectToCopy/python-flask/jenkinsfile')
+         extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+       }
+     }
+   }
+ }
+}
