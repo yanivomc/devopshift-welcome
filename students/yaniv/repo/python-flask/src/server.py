bb2941d0e641cbc49d82e639a8910d0985bfebab
Your Name <you@example.com>
2024-01-08 10:17:21 +0000

    updates

--- students/yaniv/repo/python-flask/src/server.py
+++ students/yaniv/repo/python-flask/src/server.py
@@ -56,4 +56,4 @@ def health_check():
         return f'Health check failed: {e}', 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8083)
+    app.run(debug=True, host='0.0.0.0', port=8083)
