a011994bda8a852c1d6a365d10609c61f6eaa7b5
Your Name <you@example.com>
2024-01-08 08:35:10 +0000

    1111

--- students/Roy/DSL/dslpipeline.groovy
+++ students/Roy/DSL/dslpipeline.groovy
@@ -1,21 +1,3 @@
-job('NodeJS example') { // Job NAME
-    scm { // Configure Source control management 
-        git('git://github.com/yanivomc/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@domain.com')
-        }
-    }
-    triggers { // Configure when to check for changes 
-        scm('H/5 * * * *')
-    }
-    wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-    }
-    steps { // what steps to take 
-        shell("npm install")
-    }
-}
 
 
 pipelineJob('projectx') { // broken branch config
@@ -35,6 +17,33 @@ pipelineJob('projectx') { // broken branch config
          remote { url(repo) }
          branches('elbit/jenkinsdec26') // this is fine
          scriptPath('students/Roy/pipeline/projectX/Jenkinsfile')
+         extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+       }
+     }
+   }
+ }
+}
+
+
+
+
+pipelineJob('Flask') { // broken branch config
+ def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
+
+
+ triggers {
+   scm('H/5 * * * *')
+ }
+ description("My Flask")
+
+
+ definition {
+   cpsScm {
+     scm {
+       git {
+         remote { url(repo) }
+         branches('elbit/jenkinsdec26') // this is fine
+         scriptPath('students/Roy/repo/python-flask/jenkinsfile')
          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
        }
      }
