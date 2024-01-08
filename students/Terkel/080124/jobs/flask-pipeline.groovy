38432da72d2a20d358ca129f9255555ac3669258
Terkel <terkmail@gmail.com>
2024-01-08 09:18:47 +0000

    revert

--- students/Terkel/080124/jobs/flask-pipeline.groovy
+++ students/Terkel/080124/jobs/flask-pipeline.groovy
@@ -15,9 +15,6 @@ pipeline {
                 catchError(buildResult: 'SUCCESS', message: 'python error', stageResult: 'UNSTABLE') {
                     sh 'python --version' // some block
                 }
-                catchError(buildResult: 'SUCCESS', message: 'python error', stageResult: 'UNSTABLE') {
-                    sh '/usr/lib/python3/python --version' // some block
-                }
             }
         }
 
