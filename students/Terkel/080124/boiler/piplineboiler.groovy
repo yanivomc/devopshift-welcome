938a3fca35122b1566da77a9e3bf3a7462834ff8
Terkel <terkmail@gmail.com>
2024-01-08 07:31:08 +0000

    added job, updated boiler

---
+++ students/Terkel/080124/boiler/piplineboiler.groovy
@@ -0,0 +1,21 @@
+pipelineJob('my-pipeline') { // ok
+  def repo = 'https://github.com/yanivomc/devopshift-welcome.git'
+
+  triggers {
+    scm('H/5 * * * *')
+  }
+  description("My Pipeline")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('elbit/jenkinsdec26') // this is fine
+          scriptPath('students/Terkel/080124/jobs/my-pipeline.groovy')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+      }
+    }
+  }
+}
