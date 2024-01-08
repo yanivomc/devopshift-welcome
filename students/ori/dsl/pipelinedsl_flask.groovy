2812ed9aaa008e372507fcb13e66b430ed7cbe9b
Ori Arad <you@example.com>
2024-01-08 08:31:33 +0000

    fix typo

--- students/ori/dsl/pipelinedsl_flask.groovy
+++ students/ori/dsl/pipelinedsl_flask.groovy
@@ -5,7 +5,7 @@ pipelineJob('My Generated Flask Job') { // Job NAME
                 git {
                     remote { url('https://github.com/yanivomc/devopshift-welcome.git') }
                     branches('elbit/jenkinsdec26') // this is fine
-                    scriptPath('students/ori/pipelines/day030124/jenkinsfile')
+                    scriptPath('students/ori/pipelines/day080124/jenkinsfile')
                     extensions { }  // required as otherwise it may try to tag the repo, which you may not want
                 }  
            }           
