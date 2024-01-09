28786a8a24e11cc4e6b047cecf44ef9d03ad2264
root <admin@example.com>
2024-01-09 16:20:50 +0200

    restructure

---
+++ seminars-data/docker/part-3/pythonAndUnitTest/code/messages.py
@@ -0,0 +1,9 @@
+def greet(name):
+    message = f"Hello, {name}!"
+    print(message)
+    return message
+
+def farewell(name):
+    message = f"Goodbye, {name}!"
+    print(message)
+    return message
