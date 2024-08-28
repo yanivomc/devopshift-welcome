3bb46ff1c7f63a6a24d42309438e038a7a83c9aa
root <admin@example.com>
2024-08-28 14:56:55 +0300

    updated

---
+++ welcome/jenkins/samples/slides/solutions/LibProject/vars/myLibrary.groovy
@@ -0,0 +1,14 @@
+def buildApp() {
+    echo "Now building the application..."
+    // Mock build logic here
+}
+
+def deployApp(String branchName) {
+    echo "Now deploying the application on branch: ${branchName}..."
+    // Mock deploy logic here
+}
+
+def cleanup() {
+    echo "Cleaning up after build and deployment..."
+    // Mock cleanup logic here
+}
