500d7a820363dfd2ba037d330960d8a2870be995
Your Name <you@example.com>
2024-10-28 11:18:36 +0000

    added lab104

--- code/azure/solutions/lab104/studentExtension/variables.tf
+++ code/azure/solutions/lab104/studentExtension/variables.tf
@@ -2,19 +2,19 @@
 variable "environment" {
   description = "Define the environment type: dev, staging, or prod"
   type        = string
-  default     = "prod"
+  default     = "dev"
 }
 
 # Variable to control high availability
 variable "high_availability" {
   description = "Whether to enable high availability (true or false)"
   type        = bool
-  default     = true
+  default     = false
 }
 
 # Variable to control mock database creation
 variable "create_database" {
   description = "Whether to create a mock database (true or false)"
   type        = bool
-  default     = true
+  default     = false
 }
