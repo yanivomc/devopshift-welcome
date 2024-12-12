1cafdb233ae4b902d88b3dce717c1aefd91d4d56
root <admin@example.com>
2024-12-12 11:06:08 +0200

    removed port 80

--- code/aws/solutions/lab103/network.tf
+++ code/aws/solutions/lab103/network.tf
@@ -6,12 +6,12 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   from_port   = 80
+  #   to_port     = 80
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   egress {
     from_port   = 0
