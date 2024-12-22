1114126fa37ef2b0162e00f102127db38c309a5f
root <admin@example.com>
2024-12-22 09:24:42 +0200

    Control flow labs

---
+++ code/solutions/modules/101/control-flow/rangeLoop.py
@@ -0,0 +1,2 @@
+for num in range(1, 11):
+    print(f"Square of {num} is {num**2}")
---
+++ code/solutions/modules/101/control-flow/whileLoops.py
@@ -0,0 +1,5 @@
+while True:
+    command = input("Enter a command (type 'exit' to quit): ")
+    if command.lower() == "exit":
+        break
+    print(f"You entered: {command}")
