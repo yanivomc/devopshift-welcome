a1ed59f57b582d3a203abb22e26cb59fe3795713
Your Name <you@example.com>
2025-02-06 09:35:09 +0000

    added module

--- mylab/lab500/labmodule/main.tf
+++ mylab/lab500/labmodule/main.tf
@@ -1,13 +1,19 @@
-module "outputdemo1" {
-    source = "./modules/outputdemo"
-    ec2info = "createmachine"
+
+module "gal_demo" {
+  source = "git::https://github.com/gald10102/devopshift-welcome.git//labs/labmodule/modules?ref=workshop/terraform"
+  ami = "ami-0c02fb55956c7d316"
+  machine_type = "t2.micro"
+  machine_name = "yanivomc"
 }
 
 
+# Gal env:
+# folder labs/labmodule/modules
+# git: https://github.com/gald10102/devopshift-welcome.git
 
 
-
-output "printingmpduleinfo" {
-    value = module.outputdemo1
+output "priting_gal_outputs" {
+    value = module.gal_demo
   
 }
+
