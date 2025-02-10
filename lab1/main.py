ecd79b35a59415d980110dbd92bacff44720b4b3
יונתן רדעי <radai2510@gmail.com>
2025-02-10 10:31:37 +0200

    Lab 1 Step 2

--- lab1/main.py
+++ lab1/main.py
@@ -1,9 +1,14 @@
+valid_server = {"nginx", "docker", "apache"}
 try:
     server = input("Enter a server name: ")
     if server == "":
         raise ValueError("Server name is empty.")
     if not server.isalnum():
         raise ValueError("Server name must be alphanumeric.")
+    if server not in valid_server:
+        raise ValueError("Server is not recognized.")
+    else:
+        print("Server is running.")    
 except ValueError as err:
     print(err)
     
