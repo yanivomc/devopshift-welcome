54d34016eb0a0bb2e7984492858f75462f5ffcd3
יונתן רדעי <radai2510@gmail.com>
2025-02-11 15:37:24 +0200

    Clean up lab folder

--- lab/main.py
+++ lab/main.py
@@ -43,3 +43,47 @@ def add_server(server_name: str) -> ServerStatusResponse:
 # @app.post("/users")
 # def create_user(new_user: User) -> bool:
 #     return True
+
+
+# from log import setup_logging
+# from invalid_server_error import InvalidServerError
+# from valid_servers import valid_servers
+
+# logger = setup_logging()
+
+# def check_service_status(server_name):
+#     try:
+#         if server_name == "":
+#             raise InvalidServerError("Server name is empty.")
+#         if not server_name.isalnum():
+#             raise InvalidServerError("Server name must be alphanumeric.")
+#         if server_name not in valid_servers:
+#             raise InvalidServerError("Server is not recognized.")
+#         else:
+#             return "Running"
+#     except InvalidServerError:
+#         raise ValueError
+# if __name__ == "__main__":
+#     server_name = ""
+#     while server_name != "exit":
+#             server_name = input("Enter a server name:\n")
+#             server_name = server_name.strip().lower()
+#             if server_name == "exit":
+#                 break
+#             try:
+#                 status = check_service_status(server_name)
+#                 logger.info("Valid Server name.")
+#             except ValueError as err:
+#                 logger.error("Invalid Server name.")
+
+
+# import httpx
+# import json
+# from pprint import pprint
+
+# response = httpx.get('https://jsonplaceholder.typicode.com/users')
+# pprint(response)
+# users = response.json()
+# for user in users:
+#     pprint(user['name'])
+    
