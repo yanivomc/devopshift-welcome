feab1284717eaf59b75e2b8118ebdfef721cb0ee
יונתן רדעי <radai2510@gmail.com>
2025-02-11 16:39:13 +0200

    Read from servers file

--- lab/main.py
+++ lab/main.py
@@ -3,87 +3,26 @@ import json
 import httpx
 from dataclasses import dataclass
 from datetime import datetime
-from valid_servers import valid_servers
+import modules
+
+app = FastAPI()
+servers = modules.read_server_list()
 
 @dataclass
 class ServerStatusResponse:
     server_name: str
     server_status: str | bool
 
-app = FastAPI()
-
 @app.get("/server")
 def get_server(server_name: str) -> ServerStatusResponse:
-    server_status = valid_servers.get(server_name, "Doest not exist.")
-    return ServerStatusResponse(server_name, server_status)
+    server_status = False
+    for server in servers:
+        if server.name == server_name:
+            return ServerStatusResponse(server_name, server_status)
+    return ServerStatusResponse(server_name, server_name + " Does not exist.")
 
  
 @app.post("/server")
 def add_server(server_name: str) -> ServerStatusResponse:
-    valid_servers[server_name] = True
-    return ServerStatusResponse(server_name, valid_servers[server_name])
-
-
-# @dataclass
-# class User:
-#     name:str
-#     email:str
-#     date = datetime.now()
-
-
-# @app.get("/")
-# def hello_world():
-#     return "Hello World"
-
-# @app.get("/users")
-# def get_users() ->list[User]:
-#     response = httpx.get('https://jsonplaceholder.typicode.com/users')
-#     return response.json()
-
-# @app.post("/users")
-# def create_user(new_user: User) -> bool:
-#     return True
-
-
-# from log import setup_logging
-# from invalid_server_error import InvalidServerError
-# from valid_servers import valid_servers
-
-# logger = setup_logging()
-
-# def check_service_status(server_name):
-#     try:
-#         if server_name == "":
-#             raise InvalidServerError("Server name is empty.")
-#         if not server_name.isalnum():
-#             raise InvalidServerError("Server name must be alphanumeric.")
-#         if server_name not in valid_servers:
-#             raise InvalidServerError("Server is not recognized.")
-#         else:
-#             return "Running"
-#     except InvalidServerError:
-#         raise ValueError
-# if __name__ == "__main__":
-#     server_name = ""
-#     while server_name != "exit":
-#             server_name = input("Enter a server name:\n")
-#             server_name = server_name.strip().lower()
-#             if server_name == "exit":
-#                 break
-#             try:
-#                 status = check_service_status(server_name)
-#                 logger.info("Valid Server name.")
-#             except ValueError as err:
-#                 logger.error("Invalid Server name.")
-
-
-# import httpx
-# import json
-# from pprint import pprint
-
-# response = httpx.get('https://jsonplaceholder.typicode.com/users')
-# pprint(response)
-# users = response.json()
-# for user in users:
-#     pprint(user['name'])
-    
+    modules.add_new_server(modules.Server(name=server_name, online=True, cpus=6, ram=10))
+    return ServerStatusResponse(server_name, " Server was added and now is running.")
