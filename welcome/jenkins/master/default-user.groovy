54bfe5fd76ef182b930bcef9f6b0d1810dbabc6a
yaniv <yanivomc@gmail.com>
2026-01-15 14:25:12 +0200

    refactor latest jenkins version

--- welcome/jenkins/master/default-user.groovy
+++ welcome/jenkins/master/default-user.groovy
@@ -1,14 +1,30 @@
 import jenkins.model.*
 import hudson.security.*
+import org.jenkinsci.plugins.matrixauth.PermissionEntry
+import org.jenkinsci.plugins.matrixauth.AuthorizationType
 
 def env = System.getenv()
 
 def jenkins = Jenkins.getInstance()
-jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
-jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
 
-def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
-user.save()
+// Only set security realm if not already configured (CasC might have done it)
+// Using class name check to avoid IllegalAccessError on protected inner class SecurityRealm.None
+if (jenkins.getSecurityRealm().getClass().getName().contains("None")) {
+    jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
+    def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
+    user.save()
+}
 
-jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
+// Only add admin permission if the strategy matches our expectation
+// and avoid calling setAuthorizationStrategy(new ...) which wipes CasC settings
+def authStrategy = jenkins.getAuthorizationStrategy()
+if (authStrategy instanceof GlobalMatrixAuthorizationStrategy) {
+    def adminUserId = env.JENKINS_USER
+    def adminEntry = PermissionEntry.user(adminUserId)
+    
+    // Use the modern PermissionEntry API to avoid legacy warnings
+    authStrategy.add(Jenkins.ADMINISTER, adminEntry)
+    authStrategy.add(Jenkins.READ, adminEntry)
+}
+
 jenkins.save()
--- welcome/jenkins/slave/slave.py
+++ welcome/jenkins/slave/slave.py
@@ -1,55 +1,147 @@
-from jenkins import Jenkins, JenkinsError, NodeLaunchMethod
 import os
 import signal
 import sys
-import urllib.request as url
 import subprocess
 import shutil
-import requests
 import time
+import requests
+import api4jenkins
+from urllib.parse import quote
 
 slave_jar = '/var/lib/jenkins/slave.jar'
-slave_name = os.environ['SLAVE_NAME'] if os.environ['SLAVE_NAME'] != '' else 'docker-slave-' + os.environ['HOSTNAME']
-jnlp_url = os.environ['JENKINS_URL'] + '/computer/' + slave_name + '/slave-agent.jnlp'
 slave_jar_url = os.environ['JENKINS_URL'] + '/jnlpJars/slave.jar'
-print(slave_jar_url)
 process = None
 
-def clean_dir(dir):
-    for root, dirs, files in os.walk(dir):
+def clean_dir(directory):
+    for root, dirs, files in os.walk(directory):
         for f in files:
             os.unlink(os.path.join(root, f))
         for d in dirs:
             shutil.rmtree(os.path.join(root, d))
 
+def get_jenkins_instance():
+    return api4jenkins.Jenkins(
+        os.environ['JENKINS_URL'],
+        auth=(os.environ['JENKINS_USER'], os.environ['JENKINS_PASS'])
+    )
+
+def sanitize(value):
+    """Remove unsafe characters from a string."""
+    import re
+    return re.sub(r"[^a-zA-Z0-9_-]", "", value)
+
+def slave_exists(jenkins, node_name):
+    try:
+        node = jenkins.nodes.get(node_name)
+        if node:
+            print(f"DEBUG: Node '{node_name}' exists.")
+            return True
+        else:
+            print(f"DEBUG: Node '{node_name}' does not exist or cannot be retrieved.")
+            return False
+    except KeyError:
+        print(f"DEBUG: Node '{node_name}' does not exist.")
+        return False
+
 def slave_create(node_name, working_dir, executors, labels):
-    j = Jenkins(os.environ['JENKINS_URL'], os.environ['JENKINS_USER'], os.environ['JENKINS_PASS'])
-    j.node_create(node_name, working_dir, num_executors = int(executors), labels = labels, launcher = NodeLaunchMethod.JNLP)
+    jenkins = get_jenkins_instance()
+
+    # Sanitize inputs
+    node_name = sanitize(node_name)
+    labels = sanitize(labels)
+
+    print(f"Creating node '{node_name}' with {executors} executors and labels [{labels}]...")
+
+    try:
+        if slave_exists(jenkins, node_name):
+            print(f"Node '{node_name}' already exists. Skipping creation as it is likely managed by CasC.")
+            return
+
+        # Define node configuration
+        node_config = {
+            "nodeDescription": "Automatically created by script",
+            "numExecutors": int(executors),
+            "remoteFS": working_dir,
+            "labelString": labels,
+            "mode": "NORMAL",  # NORMAL or EXCLUSIVE
+            "launcher": {"stapler-class": "hudson.slaves.JNLPLauncher"},
+            "retentionStrategy": {"stapler-class": "hudson.slaves.RetentionStrategy$Always"},
+        }
+        # Add node_config to kwargs
+        kwargs = {
+            "name": node_name,
+            "nodeDescription": node_config["nodeDescription"],
+            "numExecutors": node_config["numExecutors"],
+            "remoteFS": node_config["remoteFS"],
+            "labelString": node_config["labelString"],
+            "mode": node_config["mode"],
+            "launcher": node_config["launcher"],
+            "retentionStrategy": node_config["retentionStrategy"],
+        }
+
+        # Create the node
+        jenkins.nodes.create(**kwargs)
+        print(f"Node '{node_name}' created successfully.")
+
+    except Exception as e:
+        print(f"ERROR: Failed to create node '{node_name}': {e}")
+        raise
+
 
 def slave_delete(node_name):
-    j = Jenkins(os.environ['JENKINS_URL'], os.environ['JENKINS_USER'], os.environ['JENKINS_PASS'])
-    j.node_delete(node_name)
+    jenkins = get_jenkins_instance()
 
-def slave_download(target):
+    if slave_exists(jenkins, node_name):
+        # We don't delete if it might be managed by CasC
+        print(f"Node '{node_name}' exists. Skipping deletion to avoid conflict with CasC.")
+    else:
+        print(f"Node '{node_name}' does not exist. Skipping deletion.")
+
+def slave_download():
     if os.path.isfile(slave_jar):
         os.remove(slave_jar)
 
-    loader = url.URLopener()
-    loader.retrieve(os.environ['JENKINS_URL'] + '/jnlpJars/slave.jar', '/var/lib/jenkins/slave.jar')
+    response = requests.get(slave_jar_url, stream=True, verify=False)
+    with open(slave_jar, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
+    print("Downloaded Jenkins slave jar.")
 
-def slave_run(slave_jar, jnlp_url):
-    params = [ 'java', '-jar', slave_jar, '-jnlpUrl', jnlp_url ]
-    if os.environ['JENKINS_SLAVE_ADDRESS'] != '':
-        params.extend([ '-connectTo', os.environ['JENKINS_SLAVE_ADDRESS' ] ])
+def get_slave_secret(slave_name):
+    try:
+        url = f"{os.environ['JENKINS_URL']}/computer/{slave_name}/jenkins-agent.jnlp"
+        auth = (os.environ['JENKINS_USER'], os.environ['JENKINS_PASS'])
+        response = requests.get(url, auth=auth, verify=False)
+        if response.status_code == 200:
+            for line in response.text.splitlines():
+                if '<argument>' in line:
+                    secret = line.split('<argument>')[1].split('</argument>')[0]
+                    return secret
+        else:
+            raise Exception(f"Failed to fetch slave secret: {response.status_code} {response.reason}")
+    except Exception as e:
+        print(f"ERROR: Unable to retrieve the slave secret: {e}")
+        sys.exit(1)
 
-    if os.environ['SLAVE_SECRET'] == '':
-        params.extend([ '-jnlpCredentials', os.environ['JENKINS_USER'] + ':' + os.environ['JENKINS_PASS'] ])
-    else:
-        params.extend([ '-secret', os.environ['SLAVE_SECRET'] ])
+def slave_run(slave_jar, slave_name):
+    jenkins = get_jenkins_instance()
+
+    slave_secret = get_slave_secret(slave_name)
+    params = [
+        'java', '-jar', slave_jar, '-url', os.environ['JENKINS_URL'], '-name', slave_name, '-secret', slave_secret, '-webSocket'
+    ]
+
+    if os.environ.get('JENKINS_SLAVE_ADDRESS'):
+        params.extend(['-tunnel', os.environ['JENKINS_SLAVE_ADDRESS']])
+
+    params.append('-workDir')
+    params.append(os.getcwd())
+
     return subprocess.Popen(params, stdout=subprocess.PIPE)
 
 def signal_handler(sig, frame):
-    if process != None:
+    global process
+    if process is not None:
         process.send_signal(signal.SIGINT)
 
 signal.signal(signal.SIGINT, signal_handler)
@@ -57,34 +149,32 @@ signal.signal(signal.SIGTERM, signal_handler)
 
 def master_ready(url):
     try:
-        r = requests.head(url, verify=False, timeout=None)
-        return r.status_code == requests.codes.ok
-    except:
+        response = requests.head(url, verify=False, timeout=5)
+        return response.status_code == requests.codes.ok
+    except requests.RequestException:
         return False
 
 while not master_ready(slave_jar_url):
-    print("Master not ready yet, sleeping for 10sec!")
+    print("Master not ready yet, sleeping for 10 seconds!")
     time.sleep(10)
 
-slave_download(slave_jar)
-print ("Downloaded Jenkins slave jar.")
+slave_download()
 
-if os.environ['SLAVE_WORING_DIR']:
-    os.setcwd(os.environ['SLAVE_WORING_DIR'])
+if os.environ.get('SLAVE_WORKING_DIR'):
+    os.chdir(os.environ['SLAVE_WORKING_DIR'])
 
-if os.environ['CLEAN_WORKING_DIR'] == 'true':
+if os.environ.get('CLEAN_WORKING_DIR') == 'true':
     clean_dir(os.getcwd())
-    print ("Cleaned up working directory.")
+    print("Cleaned up working directory.")
 
-if os.environ['SLAVE_NAME'] == '':
-    slave_create(slave_name, os.getcwd(), os.environ['SLAVE_EXECUTORS'], os.environ['SLAVE_LABELS'])
-    print ("Created temporary Jenkins slave.")
+slave_name = os.environ.get('SLAVE_NAME', f"docker-slave-{os.environ.get('HOSTNAME')}")
+slave_create(slave_name, os.getcwd(), os.environ['SLAVE_EXECUTORS'], os.environ['SLAVE_LABELS'])
 
-process = slave_run(slave_jar, jnlp_url)
-print ("Started Jenkins slave with name "' + slave_name + '" and labels [' + os.environ['SLAVE_LABELS'] + '].")
+process = slave_run(slave_jar, slave_name)
+print(f"Started Jenkins slave with name '{slave_name}' and labels [{os.environ['SLAVE_LABELS']}].")
 process.wait()
 
-print ("Jenkins slave stopped.")
-if os.environ['SLAVE_NAME'] == '':
+print("Jenkins slave stopped.")
+if os.environ.get('SLAVE_NAME') == '':
     slave_delete(slave_name)
-    print ("Removed temporary Jenkins slave.")
+    print("Removed temporary Jenkins slave.")
